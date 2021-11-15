e44d8e8d3b2bdc6bc293d7b0d843ba6353ee8796
tadao kobayashi <tadao_kobayashi@diveintocode.jp>
2021-11-15 16:02:43 +0900

    taskモデルのバリデーションを修正

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,4 +1,4 @@
 class Task < ApplicationRecord
-  validates :title, presence: true, length{maximum: 20}
-  validates :content, presence: true, length{maximum: 100}
+  validates :title, presence: true, length: {maximum: 20}
+  validates :content, presence: true, length: {maximum: 100}
 end
