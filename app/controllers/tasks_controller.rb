87045d5002cd029107e77a37e1126768ec0c3b11
tadao kobayashi <tadao_kobayashi@diveintocode.jp>
2021-11-15 15:42:54 +0900

    taskコントローラの各アクションに処理を追加

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,46 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
   def index
+    @tasks = Task.all
   end
 
   def new
+    @task = Task.new
   end
 
   def create
+    @task = Task.new(task_params)
+    @task.save
+    redirect_to tasks_index_path
   end
 
   def show
   end
 
+  def edit
+  end
+
   def update
+    @btask = Task.find(params[:id])
+    if @task.update(task_params)
+      redirect_to tasks_path, notice: "タスクを編集しました"
+    else
+      render :edit
+    end
   end
 
   def destroy
+    @task.destroy
+    redirect_to tasks_path, notice:"タスクを削除しました"
+  end
+
+  private
+  def task_params
+    params.require(:task).permit(:title, :content)
+  end
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
