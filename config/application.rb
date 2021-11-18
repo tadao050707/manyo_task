63e9c709e39f00b4eae5fc2a01094bbc34399993
tadao kobayashi <tadao_kobayashi@diveintocode.jp>
2021-11-18 15:16:09 +0900

    rspec関連のコード記載場所を修正

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,16 @@ module TaskApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    
+    config.generators do |g|
+      g.test_framework :rspec,
+                       model_specs: true,
+                       view_specs: false,
+                       helper_specs: false,
+                       routing_specs: false,
+                       controller_specs: false,
+                       request_specs: false
+    end
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
@@ -19,14 +28,4 @@ module TaskApp
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
   end
-end
-
-config.generators do |g|
-  g.test_framework :rspec,
-                   model_specs: true,
-                   view_specs: false,
-                   helper_specs: false,
-                   routing_specs: false,
-                   controller_specs: false,
-                   request_specs: false
 end
