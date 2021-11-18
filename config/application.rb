a806ce178feb409b59d7c1fd6ca3623fb1ef7b1c
tadao kobayashi <tadao_kobayashi@diveintocode.jp>
2021-11-18 15:10:50 +0900

    必要のないテストファイルは作成しないように設定

--- config/application.rb
+++ config/application.rb
@@ -20,3 +20,13 @@ module TaskApp
     # config.eager_load_paths << Rails.root.join("extras")
   end
 end
+
+config.generators do |g|
+  g.test_framework :rspec,
+                   model_specs: true,
+                   view_specs: false,
+                   helper_specs: false,
+                   routing_specs: false,
+                   controller_specs: false,
+                   request_specs: false
+end
