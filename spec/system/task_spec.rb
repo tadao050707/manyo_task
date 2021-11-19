86994fdea6a09e343436ee5829115b644d333269
tadao kobayashi <tadao_kobayashi@diveintocode.jp>
2021-11-19 11:30:17 +0900

    ステップ1のsysytem spec完成

--- spec/system/task_spec.rb
+++ spec/system/task_spec.rb
@@ -36,6 +36,11 @@ RSpec.describe 'タスク管理機能', type: :system do
   describe '詳細表示機能' do
      context '任意のタスク詳細画面に遷移した場合' do
        it '該当タスクの内容が表示される' do
+				FactoryBot.create(:task)
+				visit tasks_path
+				click_link '詳細'
+				expect(page).to have_content 'test_title'
+				expect(page).to have_content 'test_content'
        end
      end
   end
